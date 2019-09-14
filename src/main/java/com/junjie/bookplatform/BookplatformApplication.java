2f795f56a0ba9604e91ec258d2fcd4dd34138c5e
modiejun <junjie981208@gmail.com>
2019-09-14 21:19:39 +0800

    Init

---
+++ src/main/java/com/junjie/bookplatform/BookplatformApplication.java
@@ -0,0 +1,13 @@
+package com.junjie.bookplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookplatformApplication.class, args);
+	}
+
+}
